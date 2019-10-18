731010be08fcf1e96fdc25b7ae07bbb57b2b3735
Apecengo <Apecengo@users.noreply.github.com>
2019-10-18 19:17:35 +0200

    Add bottom margin to footer

--- components/FooterContent.vue
+++ components/FooterContent.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="text-gray-600 container mx-auto px-4 text-xs">
+  <footer class="text-gray-600 container mx-auto px-4 text-xs mb-4">
     <p>The Monolith logo and select assets are &copy; Thriving Ventures AB. This site is not affiliated nor endorsed by Thriving Ventures AB.</p>
   </footer>
 </template>
